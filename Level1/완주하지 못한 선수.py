aabefae525e2090c19addcd53cd8725a162b426d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-05-31 21:28:52 +0900

    Level1_완주하지 못한 선수
    https://programmers.co.kr/learn/courses/30/lessons/42576

---
+++ Level1/완주하지 못한 선수.py
@@ -0,0 +1,10 @@
+participant=["mislav", "stanko", "mislav", "ana"]
+completion=["stanko", "ana", "mislav"]
+def solution(participant,completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i]!=completion[i]:
+            return participant[i]
+    return participant.pop()
+print(solution(participant,completion))
