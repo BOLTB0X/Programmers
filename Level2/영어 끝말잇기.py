826c6f6266dc805fa3113f27785f7653490a4593
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-08-17 13:22:39 +0900

    영어 끝말잇기
    https://programmers.co.kr/learn/courses/30/lessons/12981?language=python3

--- Level2/영어 끝말잇기.py
+++ Level2/영어 끝말잇기.py
@@ -1,14 +1,10 @@
-n=2
-words=["hello", "one", "even", "never", "now", "world", "draw"]
-#중복 된거 말하거나 틀리면 탈락
 def solution(n, words):
     answer = []
     check=[words[0]]
     for i in range(1,len(words)):
         if words[i] in check:
                 answer.append((i%n)+1)
-                answer.append((i+1)//n)
-                check.append(words[i])
+                answer.append((i//n)+1)
                 break
         elif words[i][0]==check[i-1][-1]:
             check.append(words[i])
@@ -20,4 +16,3 @@ def solution(n, words):
         answer.append(0)
         answer.append(0)
     return answer
-print(solution(n,words))
