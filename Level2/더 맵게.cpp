1ac57f8607ea337ec451e90efd0f70fad5390084
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-04 13:20:43 +0900

    더 맵게
    https://programmers.co.kr/learn/courses/30/lessons/42626?language=cpp

--- Level2/더 맵게.cpp
+++ Level2/더 맵게.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
+
 using namespace std;
 
 struct compare {
@@ -12,20 +12,24 @@ struct compare {
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    priority_queue<int,vector<int>,compare> pq;
+    //최소 힙
+    priority_queue<int, vector<int>, compare> pq;
 
-    for (int i = 0; i < scoville.size(); i++) {
+    for (int i = 0; i < scoville.size(); i++)
         pq.push(scoville[i]);
-    }
+
+    //2개이상있고 K 이상이 될때까지 반복
     while (pq.size() > 1 && pq.top() < K) {
-        int tmp_fir = pq.top();
+        int fir = pq.top();
         pq.pop();
-        int tmp_sec = pq.top();
+        int sec = pq.top();
         pq.pop();
-        pq.push(tmp_fir + (tmp_sec * 2));
+        pq.push(fir + (sec * 2));
         answer++;
     }
-    if (pq.top() < K)
+    //만약 반복문 후에도 스코빌지수가 낮다면
+    if (pq.top() < K) {
         answer = -1;
+    }
     return answer;
 }
