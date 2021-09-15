97d1b9121baa7d587f6d598560f407dac0b3b4b2
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-16 00:10:01 +0900

    2 x n 타일링
    https://programmers.co.kr/learn/courses/30/lessons/12900

---
+++ Level3/2 x n 타일링.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    dp = []
+    dp.append(1)
+    dp.append(2)
+    for i in range(2,n):
+        dp.append((dp[i-1]+dp[i-2]))
+    answer=dp[-1];
+    return answer
