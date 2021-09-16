4ba5d35e37f37bff6c5195cacc710e57bfafac04
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-16 10:01:31 +0900

    3진법 뒤집기
    https://programmers.co.kr/learn/courses/30/lessons/68935?language=cpp

--- Level1/3진법 뒤집기.cpp
+++ Level1/3진법 뒤집기.cpp
@@ -3,29 +3,30 @@
 
 using namespace std;
 
-int pow(int a, int b) {
-    int result = 1;
-    if (b == 0)
-        return 1;
-    for (int i = 1; i <= b; i++) {
-        result *= a;
+//3진법 변환
+string trans_num(int n) {
+    string str = "";
+    while (n >= 1) {
+        int r = (n % 3);
+        str += (char)(r + '0');
+        n /= 3;
+    }
+    return str;
+}
+
+//십진법 반환
+int get_number(string s) {
+    int result = 0;
+    int mul = 1;
+    for (int i = s.length() - 1; i >= 0; i--) {
+        result += (s[i] - '0') * mul;
+        mul *= 3;
     }
     return result;
 }
 
 int solution(int n) {
     int answer = 0;
-    string tmp_n;
-
-    while (n>=1) {
-        tmp_n += (char)(n % 3) + '0';
-        n /= 3;
-    }
-    int idx = 1;
-    for (int i = tmp_n.length()-1; i >=0 ; i--) {
-        answer += (tmp_n[i]-'0')*idx;
-        idx *= 3;
-    }
-
+    answer = get_number(trans_num(n));
     return answer;
 }
