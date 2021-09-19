c04d5b2ca5fd4e4fb826c4b7268efe0d6fc9f942
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-19 14:56:19 +0900

    124 나라의 숫자
    https://programmers.co.kr/learn/courses/30/lessons/12899?language=cpp

---
+++ Level2/124 나라의 숫자.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    //3진법연산
+    //단 1,2,4 이므로 0,1,2에서 0일때 몫-1해줘야함
+    while (n >= 1) {
+        int r = n % 3;
+        n /= 3;
+        if (r == 0) {
+            answer = "4" + answer;
+            n--;
+        }
+        else if (r == 1)
+            answer = "1" + answer;
+        else if (r == 2)
+            answer = "2" + answer;
+    }
+    return answer;
+}
