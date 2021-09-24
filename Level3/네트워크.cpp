5c2e7762f9dc13b2d0a31bc25ea67311002d13a8
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-24 23:44:06 +0900

    네트워크
    https://programmers.co.kr/learn/courses/30/lessons/43162

--- Level3/네트워크.cpp
+++ Level3/네트워크.cpp
@@ -3,23 +3,27 @@
 
 using namespace std;
 
-void DFS(int start, int n, vector<vector<int>>& computers, vector<bool>& visited) {
-    visited[start] = true;
-    for (int i = 0; i < n; i++) {
-        if (!visited[i] && computers[start][i] != 0) {
-            DFS(i, n, computers, visited);
-        }
+//DFS로 그래프 탐색
+void DFS(int start, int n, vector<vector<int>> &computers, vector<bool> &visited) {
+    visited[start]=true;
+    for (int i=0;i<computers[start].size();i++) {
+        //미방문이고 1인 경우
+        if(!visited[i] && computers[start][i] == 1) 
+            DFS(i,n,computers,visited);
     }
-    return;
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    vector<bool> visited(n, false);
-    for (int i = 0; i < n; i++) {
+    //방문 벡터 초기화
+    vector<bool> visited(n,false);
+    //정점갯수만큼 돌림
+    for (int i=0;i<n;i++) {
+        //미방문일시
         if (!visited[i]) {
+            //네트워크 카운트 이제 이에 연결된 노드를은 DFS로 처리
             answer++;
-            DFS(i, n, computers, visited);
+            DFS(i,n,computers,visited);
         }
     }
     return answer;
