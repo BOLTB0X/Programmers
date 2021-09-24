772484cdbff5ef7110b24eeef2924c0cf1398137
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-25 00:00:38 +0900

    가장 큰 수
    https://programmers.co.kr/learn/courses/30/lessons/42746?language=cpp

--- Level2/가장 큰 수.cpp
+++ Level2/가장 큰 수.cpp
@@ -10,15 +10,20 @@ bool compare(const string& a, const string& b) {
 
 string solution(vector<int> numbers) {
     string answer = "";
-    vector<string> tmp_arr;
-    
+    vector<string> v;
+    //벡터 탐색
+    //문자열로 변환하여 벡터에 삽입
     for (int i = 0; i < numbers.size(); i++) {
-        tmp_arr.push_back(to_string(numbers[i]));
+        v.push_back(to_string(numbers[i]));
     }
-    sort(tmp_arr.begin(), tmp_arr.end(), compare);
-    for (int i = 0; i < tmp_arr.size(); i++) {
-        answer += tmp_arr[i];
+    //정렬조건 수정후 정렬
+    sort(v.begin(), v.end(), compare);
+
+    //순차적으로 문자열 answer에 더해줌
+    for (int i = 0; i < v.size(); i++) {
+        answer += v[i];
     }
+    //첫번째 자리가 0일수도 있으므로
     if (answer[0] == '0')
         return "0";
     return answer;
