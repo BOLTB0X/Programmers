291cb047f9686156117704d3207bfe1fe9c3b79f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-30 20:49:11 +0900

    카펫
    https://programmers.co.kr/learn/courses/30/lessons/42842?language=cpp

--- Level2/카펫.cpp
+++ Level2/카펫.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
+    //먼저 갈+노로 시작
     int width = brown + yellow;
+    //절반으로 비교
     for (int i = width / 2; i > 0; i--) {
+        //나누어 떨어진다면
         if (width % i == 0) {
             int row = i;
             int col = width / i;
+            //-2
             if ((row-2)*(col-2) == yellow) {
                 answer.push_back(row);
                 answer.push_back(col);
