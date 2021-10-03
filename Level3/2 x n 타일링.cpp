0d9b481324d5a95f4a56fa5476a69529d37f2286
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-03 15:10:46 +0900

    2 x n 타일링
    https://programmers.co.kr/learn/courses/30/lessons/12900?language=cpp

--- Level3/2 x n 타일링.cpp
+++ Level3/2 x n 타일링.cpp
@@ -9,6 +9,7 @@ int solution(int n) {
     int answer = 0;
     dp[1] = 1;
     dp[2] = 2;
+    //3부터 연산
     for (int i = 3; i <= n; i++)
         dp[i] = (dp[i - 1] + dp[i - 2] % 1000000007);
     answer = dp[n];
