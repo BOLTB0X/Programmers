f2ca756f6adebe89bbee39e670e5d9db7bcbbe86
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-03 14:10:15 +0900

    네트워크
    https://programmers.co.kr/learn/courses/30/lessons/43162

--- Level3/네트워크.cpp
+++ Level3/네트워크.cpp
@@ -3,28 +3,35 @@
 
 using namespace std;
 
-//DFS로 그래프 탐색
-void DFS(int start, int n, vector<vector<int>> &computers, vector<bool> &visited) {
-    visited[start]=true;
-    for (int i=0;i<computers[start].size();i++) {
-        //미방문이고 1인 경우
-        if(!visited[i] && computers[start][i] == 1) 
-            DFS(i,n,computers,visited);
+//깊이 우선
+void DFS(int start, int n, vector<vector<int>>& computers, vector<bool>& visited) {
+    //방문체크
+    visited[start] = true;
+    //정점 갯수만큼 순회
+    for (int i = 0; i < n; i++) {
+        //재방문이면
+        if (visited[i])
+            continue;
+        //연결되어있지 않다면
+        if (computers[start][i] == 0)
+            continue;
+        //DFS 호출
+        DFS(i, n, computers, visited);
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    //방문 벡터 초기화
-    vector<bool> visited(n,false);
-    //정점갯수만큼 돌림
-    for (int i=0;i<n;i++) {
-        //미방문일시
+    //체크리스트 초기화
+    vector<bool> visited(n, false);
+    //네트워크의 갯수는 각 정점을 for문으로 순회하여 
+    //미방문 정점을 발견하여 DFS하는 갯수와 같다.
+    for (int i = 0; i < n; i++) {
         if (!visited[i]) {
-            //네트워크 카운트 이제 이에 연결된 노드를은 DFS로 처리
             answer++;
-            DFS(i,n,computers,visited);
+            DFS(i, n, computers, visited);
         }
     }
+    //정답 반환
     return answer;
 }
