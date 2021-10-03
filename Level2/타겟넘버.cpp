81b8df2f65438388677a39c7405c7f2ded3822e1
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-03 14:21:50 +0900

    타겟 넘버
    https://programmers.co.kr/learn/courses/30/lessons/43165

--- Level2/타겟넘버.cpp
+++ Level2/타겟넘버.cpp
@@ -1,24 +1,31 @@
 #include <vector>
 
 using namespace std;
-//답
-int answer = 0;
 
-void DFS(vector<int> &arr,int tot, int target, int idx) {
-    //탈출 조건
-    //기존 벡터의 길이까지 반복하면
-    if (idx == arr.size()) {
-        //총합이 타겟과 같다면
-        if (tot == target)
-            answer++;
+//횟수 초기화
+int cnt = 0;
+
+void DFS(int tot, vector<int>& numbers, int target, int depth) {
+    //깊이가 벡터길이까지 도달했다면 == 탈출조건
+    if (depth == numbers.size()) {
+        //총합이 타켓과 같다면
+        if (tot == target) {
+            //카운트
+            cnt++;
+        }
         return;
     }
-    DFS(arr, tot + arr[idx], target, idx + 1);
-    DFS(arr, tot - arr[idx], target, idx + 1);
+    //더하거나 뺄경우를 DFS 호출
+    DFS(tot + numbers[depth], numbers, target, depth + 1);
+    DFS(tot - numbers[depth], numbers, target, depth + 1);
 }
 
 int solution(vector<int> numbers, int target) {
-    //DFS 탐색 시작
-    DFS(numbers,0, target, 0);
+    int answer = 0;
+
+    //백트래킹 기법으로 완전탐색
+    DFS(0, numbers, target, 0);
+    //카운팅된 cnt를 answer에 넣어줌
+    answer = cnt;
     return answer;
 }
