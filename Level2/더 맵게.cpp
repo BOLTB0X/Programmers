7bec6d9564f99a43b9f64611fe1040794240efc0
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-04 15:34:16 +0900

    더 맵게
    https://programmers.co.kr/learn/courses/30/lessons/42626

--- Level2/더 맵게.cpp
+++ Level2/더 맵게.cpp
@@ -4,33 +4,34 @@
 
 using namespace std;
 
-//최소 힙
+//최소 힙을 위한 조건
 struct compare {
     bool operator() (int x, int y) {
-        return x>y;
+        return x > y;
     }
 };
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
     //최소 힙
-    priority_queue<int,vector<int>,compare> pq;
-    
-    for (int i=0;i<scoville.size();i++) 
+    priority_queue<int, vector<int>, compare> pq;
+
+    //스코빌 지수 값을 우선순위 큐에 삽입
+    for (int i = 0; i < scoville.size(); i++)
         pq.push(scoville[i]);
-    
+
     //힙이 원소가 2개이상 이고 K 이상이 될때까지 반복
-    while (pq.size() >1 && pq.top()<K) {
-        int fir=pq.top();
+    while (pq.size() > 1 && pq.top() < K) {
+        int fir = pq.top();
         pq.pop();
-        int sec=pq.top();
+        int sec = pq.top();
         pq.pop();
-        pq.push(fir+(sec*2));
+        pq.push(fir + (sec * 2));
         answer++;
     }
     //만약 반복문 후에도 스코빌지수가 낮다면
-    if (pq.top() < K){
-        answer=-1;
+    if (pq.top() < K) {
+        answer = -1;
     }
     return answer;
 }
