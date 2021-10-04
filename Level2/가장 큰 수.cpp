9470dbf6fa629f8d17b767568d999dd8cf8daaf1
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-04 15:47:02 +0900

    가장 큰 수
    https://programmers.co.kr/learn/courses/30/lessons/42746

--- Level2/가장 큰 수.cpp
+++ Level2/가장 큰 수.cpp
@@ -4,8 +4,9 @@
 
 using namespace std;
 
-bool compare(const string& a, const string& b) {
-    return a + b > b + a;
+//정렬 기준 수정
+bool compare (const string &a,const string &b) {
+    return a+b>b+a;
 }
 
 string solution(vector<int> numbers) {
@@ -13,17 +14,17 @@ string solution(vector<int> numbers) {
     vector<string> v;
     //벡터 탐색
     //문자열로 변환하여 벡터에 삽입
-    for (int i = 0; i < numbers.size(); i++) {
+    for (int i=0;i<numbers.size();i++) {
         v.push_back(to_string(numbers[i]));
     }
     //정렬조건 수정후 정렬
-    sort(v.begin(), v.end(), compare);
-
+    sort(v.begin(),v.end(),compare);
+    
     //순차적으로 문자열 answer에 더해줌
-    for (int i = 0; i < v.size(); i++) {
+    for (int i=0;i<v.size();i++) {
         answer += v[i];
     }
-    //첫번째 자리가 0일수도 있으므로
+    //첫 번째 원소가 0이면 답 자체가 0
     if (answer[0] == '0')
         return "0";
     return answer;
