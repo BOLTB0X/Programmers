6b98e7a687293f20668308ad296e846c62d6c3ea
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-07 21:24:40 +0900

    프린터
    https://programmers.co.kr/learn/courses/30/lessons/42587

--- Level2/프린터.cpp
+++ Level2/프린터.cpp
@@ -5,31 +5,35 @@
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
-    queue<pair<int, int>> q; 
+    queue<pair<int, int>> q;
+    //최대
     priority_queue<int> pq;
+    //기존 자리
     int idx = 0;
-    
-    //q와 pq 값 할당
-    for (const auto& p : priorities) {
-        q.push({ idx++,p }); // 인덱스와 우선도
-        pq.push(p); //우선순위 큐에 우선도 삽입
+    //우선순위벡터를 큐와 우선순위큐에 넣어줌
+    for (int i = 0; i < priorities.size(); i++) {
+        q.push({ idx++,priorities[i] });
+        pq.push(priorities[i]);
     }
-    //나오는 순서
-    int order = 1; 
+
+    //반복문으로 정답반환
+    int answer = 1;
     while (!q.empty()) {
-        pair<int, int> cur = q.front(); //큐 최상단
-        //우선순위 큐의 상단이 작다면
-        if (pq.top() <= cur.second) {
-            //최상단이 원하는 위치이면
+        pair<int, int> cur = q.front();
+        //우선순위 >= 현위치
+        if (cur.second >= pq.top()) {
+            //원하는 위치이면 반환
             if (cur.first == location) {
-                return order;
+                return answer;
             }
+            //아니면 우선순위 뽑아줌
             q.pop();
             pq.pop();
-            order++;
+            //순번도 증가
+            answer++;
         }
+        //최하단으로 이동
         else {
-            //현재 위치를 제일 마지막으로
             q.push(cur);
             q.pop();
         }
