858d57fcc05461fa4a8bbcd0a3d6a6292d999b51
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-09 12:13:52 +0900

    가장 큰 정사각형 찾기

--- Level2/가장 큰 정사각형 찾기.cpp
+++ Level2/가장 큰 정사각형 찾기.cpp
@@ -19,7 +19,9 @@ int solution(vector<vector<int>> board) {
 
     for (int i = 1; i < row; i++) {
         for (int j = 1; j < col; j++) {
+            //0인 경우에는 정사각형이 만들어지지 않으므로 1인 경우만 계산
             if (board[i][j] == 1) {
+                //왼쪽점, 위쪽점, 왼쪽 대각선에 있는 점 비교 수행
                 board[i][j] = min(board[i][j - 1], board[i - 1][j]);
                 board[i][j] = min(board[i - 1][j - 1], board[i][j]) + 1;
                 answer = max(answer, board[i][j]);
