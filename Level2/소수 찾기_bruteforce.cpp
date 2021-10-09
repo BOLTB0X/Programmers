020b8554d313741064f20ca2be08c3def5623f6a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-09 12:52:03 +0900

    소수 찾기
    https://programmers.co.kr/learn/courses/30/lessons/42839

--- Level2/소수 찾기_bruteforce.cpp
+++ Level2/소수 찾기_bruteforce.cpp
@@ -4,11 +4,14 @@
 
 using namespace std;
 
-bool is_prime(int n) {
-    if (n == 1 || n == 0)
+int cnt = 0;
+
+//소수판별
+bool is_prime(int num) {
+    if (num == 1 || num == 0)
         return false;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0)
             return false;
     }
     return true;
@@ -16,17 +19,16 @@ bool is_prime(int n) {
 
 int solution(string numbers) {
     int answer = 0;
+    int str_len = numbers.length();
     vector<char> v;
-    //문자열 원소를 벡터에 삽입
-    for (int i = 0; i < numbers.size(); i++)
+
+    //문자형 벡터에 삽입
+    for (int i = 0; i < str_len; ++i)
         v.push_back(numbers[i]);
 
-    //벡터 오름차순 정렬
+    //순열 전 정렬
     sort(v.begin(), v.end());
-
-    //v벡터로 만들 수 있는 경의 수를 담을 벡터
     vector<int> per;
-    //경우의 수 탐색 시작
     do {
         string tmp = "";
         for (int i = 0; i <= v.size(); i++) {
@@ -37,13 +39,13 @@ int solution(string numbers) {
     } while (next_permutation(v.begin(), v.end()));
     //순열 벡터 정렬
     sort(per.begin(), per.end());
-    //중복값 제거 
+    //중복값 제거
     per.erase(unique(per.begin(), per.end()), per.end());
 
-    for (int i = 0; i < per.size(); i++) {
-        if (is_prime(per[i])) {
+    //마지막 만들어진 per의 소수인지 판단
+    for (int i = 0; i < per.size(); ++i) {
+        if (is_prime(per[i]))
             answer++;
-        }
     }
     return answer;
 }
