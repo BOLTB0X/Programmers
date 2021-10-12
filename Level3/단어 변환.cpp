e4a827a8d74fc8de9dbabcdf5b7bbf518146b5a3
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-12 22:11:55 +0900

    단어 변환
    https://programmers.co.kr/learn/courses/30/lessons/43163

--- Level3/단어 변환.cpp
+++ Level3/단어 변환.cpp
@@ -8,61 +8,61 @@ int result = 51;
 
 //최소
 int min(int a, int b) {
-    return a < b ? a : b;
+    return a<b?a:b;
 }
 
-bool is_trans(string &a, string &b) {
-    //각 문자열의 길이는 같으므로
-    int trans_cnt = 0;
-    for (int i = 0; i < a.length(); i++) {
-        if (a[i] != b[i])
-            trans_cnt++;
-        //두 번 변환해야하면 
-        if (trans_cnt == 2)
+//단어 체크
+bool is_trans(string &str1, string &str2) {
+    int cnt=0;
+    //글자수는 같으므로
+    for (int i=0;i<str1.size();++i) {
+        //다를 경우 카운트
+        if (str1[i] != str2[i]) {
+            cnt++;
+        }
+        //2개부터는 안되므로 거짓 반환
+        if (cnt == 2)
             return false;
     }
     return true;
 }
 
 //깊이 우선
-void DFS(string start, string target, vector<string>& words, vector<bool>& visited, int depth) {
-    //탈출 조건
+void DFS(vector<string> &words, vector<bool> &visited, string start, string target, int depth) {
+    //도달 한다면
     if (start == target) {
-        result = min(result, depth);
+        //최소값 셋팅
+        result = min(result,depth);
         return;
     }
-
-    //각 단어벡터 순회
-    for (int i = 0; i < words.size(); i++) {
-        //변환가능한지
-        if (is_trans(start, words[i])) {
-            //재방문이면
+    //탐색
+    for (int i=0;i<words.size();++i) {
+        //만약 변환이 가능하다면
+        if (is_trans(start,words[i])) {
+            //방문 한 경우 예외
             if (visited[i])
                 continue;
-            //방문체크
-            visited[i] = true;
-            //DFS호출
-            DFS(words[i], target, words, visited, depth + 1);
-            //완전탐색이므로 다시 미방문 처리
-            visited[i] = false;
+            //방문처리
+            visited[i]=true;
+            DFS(words,visited,words[i],target,depth+1);
+            visited[i]=false;
         }
     }
+    return;
 }
 
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
-    //먼저 체크리스트 초기화
-    vector<bool> visited(words.size(), false);
+    //방문리스트 생성 및 초기화
+    vector<bool> visited(words.size(),false);
     
-    //DFS호출
-    DFS(begin, target, words, visited, 0);
-    
-    //결과 변수 값 체크
+    //DFS로 탐색 시작
+    DFS(words,visited,begin,target,0);
+
+    //변환 못하던 하던 타겟에 도달하지 못하면
     if (result == 51)
         answer = 0;
     else
-        answer = result;
-    
-    //정답 반환
+        answer=result;
     return answer;
 }
