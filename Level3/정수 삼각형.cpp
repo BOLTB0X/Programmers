0a3e18d04121150555e673a88407489ba4216ea7
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-12 20:47:50 +0900

    정수 삼각형
    https://programmers.co.kr/learn/courses/30/lessons/43105

--- Level3/정수 삼각형.cpp
+++ Level3/정수 삼각형.cpp
@@ -3,26 +3,51 @@
 
 using namespace std;
 
+//최대
 int max(int a, int b) {
     return a > b ? a : b;
 }
 
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
+    //동적할당 시작
+    int** dp;
+    int size = triangle.size();
 
-    int dp[501][501] = { 0, };
+    //동적할당
+    dp = new int* [size + 1];
+    for (int i = 0; i <= size; ++i) {
+        dp[i] = new int[size + 1];
+    }
+    //초기화
+    for (int i = 0; i <= size; ++i) {
+        for (int j = 0; j <= size; ++j) {
+            dp[i][j] = 0;
+        }
+    }
+    //최상단 셋팅
     dp[1][1] = triangle[0][0];
 
-    for (int r = 2; r <= triangle.size(); r++) {
-        for (int c = 1; c <= r; c++) {
+    //보텀업
+    for (int r = 2; r <= size; ++r) {
+        for (int c = 1; c <= r; ++c) {
             dp[r][c] = max(dp[r - 1][c - 1], dp[r - 1][c]) + triangle[r - 1][c - 1];
         }
     }
-    int max_value = dp[triangle.size()][1];
-    for (int i = 2; i <= triangle.size(); i++) {
-        if (max_value < dp[triangle.size()][i])
-            max_value = dp[triangle.size()][i];
+
+    //최댓값 찾기
+    //최하단
+    int max_value = dp[size][1];
+    //탐색하며 최댓값을 찾을 경우 교체
+    for (int i = 2; i <= size; ++i) {
+        if (dp[size][i] > max_value)
+            max_value = dp[size][i];
     }
     answer = max_value;
+
+    //메모리 해제
+    for (int i = 0; i <= size; ++i)
+        delete[] dp[i];
+    delete[] dp;
     return answer;
 }
