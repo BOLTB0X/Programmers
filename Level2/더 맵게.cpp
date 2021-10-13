b406178ff2198db7446c1e04068fff7d9064e789
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-13 20:18:38 +0900

    더 맵게
    https://programmers.co.kr/learn/courses/30/lessons/42626

--- Level2/더 맵게.cpp
+++ Level2/더 맵게.cpp
@@ -4,32 +4,36 @@
 
 using namespace std;
 
-//최소 힙을 위한 조건
+//최소 힙을 위한
 struct compare {
-    bool operator() (int x, int y) {
-        return x > y;
+    bool operator() (int& a, int& b) {
+        return a > b;
     }
 };
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    //최소 힙
+    //우선순위 큐 선언
     priority_queue<int, vector<int>, compare> pq;
-
-    //스코빌 지수 값을 우선순위 큐에 삽입
-    for (int i = 0; i < scoville.size(); i++)
+    //우선순위 큐에 넣어줌
+    for (int i = 0; i < scoville.size(); ++i)
         pq.push(scoville[i]);
 
-    //힙이 원소가 2개이상 이고 K 이상이 될때까지 반복
-    while (pq.size() > 1 && pq.top() < K) {
-        int fir = pq.top();
+    //스코빌 지수가 맞춰질때
+    //섞는 조건이 2개가 필요하므로
+    //즉 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복해야함
+    while (pq.top() < K && pq.size() > 1) {
+        //문제 조건
+        int fir_elem = pq.top();
         pq.pop();
-        int sec = pq.top();
+        int sec_elem = pq.top();
         pq.pop();
-        pq.push(fir + (sec * 2));
+        //섞은 음식의 스코빌 지수 
+        pq.push(fir_elem + (sec_elem * 2));
         answer++;
     }
-    //만약 반복문 후에도 스코빌지수가 낮다면
+
+    //만약 최상단이 아직도 K미만이면
     if (pq.top() < K) {
         answer = -1;
     }
