0763d17444f528afa52006c353779f6778b54551
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-14 12:42:25 +0900

    기능 개발
    https://programmers.co.kr/learn/courses/30/lessons/42586

--- Level2/기능개발.cpp
+++ Level2/기능개발.cpp
@@ -5,43 +5,40 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    //남은 진행률
-    vector<int> rest_p;
-    //걸리는 시간
-    vector<int> t;
+    //100 - 진도율 
+    vector<int> rest_progresses;
+    //걸리는 일수
+    vector<int> day;
     
-    //남은 진행률 표시
     for (int i=0;i<progresses.size();++i) 
-        rest_p.push_back(100-progresses[i]);
+        rest_progresses.push_back(100-progresses[i]);
     
-    //남은 진행률이 몇일 걸리는 지 확인
-    for (int i=0;i<rest_p.size();++i) {
-        //딱 나눠 떨어져 끝나면
-        if (rest_p[i] % speeds[i] == 0) 
-            t.push_back(rest_p[i] / speeds[i]);
-        //그 외
-        else
-            t.push_back((rest_p[i] / speeds[i])+1);
+    //남은 진도율을 알았으니 스피드와 나누어떨어지는 것을
+    //비교하며 걸리는 일수를 저장함
+    for (int i=0;i<rest_progresses.size();++i) {
+        if (rest_progresses[i] % speeds[i] == 0)
+            day.push_back(rest_progresses[i] / speeds[i]);
+        else 
+            day.push_back((rest_progresses[i] / speeds[i])+1);
     }
-    //비교 날 셋팅
-    int cmp_day = t[0];
-    int cnt=1;
     
-    //각 기간 내 같이 배포되는 수 확인
-    for (int i=1;i<t.size();++i) {
-        //앞에 일이 끝나지 않은 것
-        //즉 큰 경우
-        if (cmp_day >= t[i])
+    //비교용
+    int compare = day[0];
+    int cnt=1;
+    for (int i=1;i<day.size();++i) {
+        //앞에 진도율이 끝나지 않았다면
+        //작업 한 후 배포이므로
+        if (compare >= day[i]) 
             cnt++;
-        //드디어 작다면
         else {
+            //배포 후 비교랑 같이 배포되는 프로그램 갯수 초기화
             answer.push_back(cnt);
-            cmp_day=t[i];
             cnt=1;
+            compare=day[i];
         }
     }
-    //마지막은 처리가 안되니
+    //마지막은 처리가 안되니 
     answer.push_back(cnt);
-    //정답 반환
+    
     return answer;
 }
