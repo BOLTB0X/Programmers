4e7c30ce3d40821abb02650864ee267b6b78d61e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-14 15:59:02 +0900

    가장 큰 수
    https://programmers.co.kr/learn/courses/30/lessons/42746

--- Level2/가장 큰 수.cpp
+++ Level2/가장 큰 수.cpp
@@ -4,28 +4,31 @@
 
 using namespace std;
 
-//정렬 기준 수정
-bool compare (const string &a,const string &b) {
-    return a+b>b+a;
+//정렬조건 수정 즉 
+//앞뒤로 더한 값이 큰거를 앞으로
+bool compare(const string& a, const string& b) {
+    return a+b > b+a;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
     vector<string> v;
-    //벡터 탐색
-    //문자열로 변환하여 벡터에 삽입
-    for (int i=0;i<numbers.size();i++) {
-        v.push_back(to_string(numbers[i]));
-    }
-    //정렬조건 수정후 정렬
-    sort(v.begin(),v.end(),compare);
     
-    //순차적으로 문자열 answer에 더해줌
-    for (int i=0;i<v.size();i++) {
+    //각 문자열을 정수로 변환하여 벡터에 삽입
+    for (int i=0;i<numbers.size();++i) 
+        v.push_back(to_string(numbers[i]));
+    
+    //정렬조건 수정 후 정렬
+    sort(v.begin(),v.end(),compare);
+   
+    //v벡터가 정렬되었으므로 정답에 차례로 넣어준다.
+    for (int i=0;i<v.size();++i)
         answer += v[i];
-    }
-    //첫 번째 원소가 0이면 답 자체가 0
+    
+    //만약 맨 앞자리가 0이면
     if (answer[0] == '0')
         return "0";
+    
+    //정답 반환
     return answer;
 }
