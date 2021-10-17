e22a536740eb049c925cc64ab028ee2a12902686
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-17 19:07:16 +0900

    N개의 최소공배수
    https://programmers.co.kr/learn/courses/30/lessons/12953

--- Level2/N개의 최소공배수.cpp
+++ Level2/N개의 최소공배수.cpp
@@ -12,15 +12,17 @@ int gcd(int a, int b) {
 
 //최소공배수
 int lcm(int a, int b) {
-    return (a*b / gcd(a,b));
+    return a*b/gcd(a,b);
 }
 
 int solution(vector<int> arr) {
+    //배열의 첫 원소를 셋팅
     int answer = arr[0];
     
-    //순차적으로 비교
-    for (int i=1; i<arr.size(); ++i) 
+    //각 원소들을 순회하며 최소공배수를 구함
+    for (int i=1;i<arr.size();++i) {
         answer = lcm(answer,arr[i]);
+    }
     
     return answer;
 }
