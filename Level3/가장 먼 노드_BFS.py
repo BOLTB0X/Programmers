6a3ee6b2c677d38af124c2a18c8b3f95b66d0798
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-05 15:16:45 +0900

    가장 먼 노드_BFS
    https://programmers.co.kr/learn/courses/30/lessons/49189?language=python3

---
+++ Level3/가장 먼 노드_BFS.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+#너비 우선 그래프 
+def BFS(graph, dist):
+    q=deque()
+    dist[1]=1
+    q.append(1)
+
+    while q:
+        cur = q.popleft()
+
+        for i in graph[cur]:
+            if dist[i] == -1:
+                q.append(i)
+                dist[i] = dist[cur]+1
+
+
+def solution(n, edge):
+    answer = 0
+    #거리정보를 담는 리스트
+    dist = [-1 for _ in range(len(edge)+1)]
+    #무향그래프
+    graph = [[] for _ in range(len(edge)+1)]
+
+    for i in range(len(edge)):
+        graph[edge[i][0]].append(edge[i][1])
+        graph[edge[i][1]].append(edge[i][0])
+    
+    #BFS 호출
+    BFS(graph,dist)
+
+    max_value = max(dist)
+    #1번 노드부터 가장 먼 노드 갯수 카운트
+    for d in dist:
+        if d == max_value:
+            answer += 1
+
+    return answer
