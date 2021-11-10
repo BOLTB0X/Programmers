14ae23926e146a045edc321f3498a336e4101f5b
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-11 00:17:16 +0900

    큰 수 만들기
    https://programmers.co.kr/learn/courses/30/lessons/42883

--- Level2/큰 수 만들기.cpp
+++ Level2/큰 수 만들기.cpp
@@ -5,14 +5,17 @@ using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
+
     //반복한 길이
     int number_size = number.size() - k;
     int start = 0;
+    
     //완전탐색 스타일로
     for (int i = 0; i < number_size; i++) {
         //최대값 셋팅
         char max_number = number[start];
         int max_idx = start;
+        
         //셋팅된 값 부터 k거리만큼 탐색
         for (int j = start; j <= k + i; j++) {
             if (max_number < number[j]) {
@@ -20,6 +23,7 @@ string solution(string number, int k) {
                 max_idx = j;
             }
         }
+
         //탐색 후
         start = max_idx + 1;
         answer += max_number;
