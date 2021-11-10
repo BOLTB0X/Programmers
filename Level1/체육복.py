a7f2dba285022cd5b946d525433b125830382da9
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-11 00:13:53 +0900

    체육복
    https://programmers.co.kr/learn/courses/30/lessons/42862?language=python3

--- Level1/체육복.py
+++ Level1/체육복.py
@@ -1,15 +1,16 @@
-#체육복
 def solution(n,lost,reserve):
-    slost=list(set(lost)-set(reserve)) #중복제거
-    sreserve=list(set(reserve)-set(lost)) #중복제거
-    #여벌 기준으로 생각
-    for re in sreserve:
-        if re-1 in slost: #여벌의 왼쪽에 분실이 있는지 확인
-            slost.remove(re-1)
-        elif re+1 in slost: #여벌의 오른쪽에 분실이 있는지 확인
-            slost.remove(re+1)
-    return (n-len(slost)) #학생수 - 빼기 분실한 넘 == 운동가능 수
-n=int(input())
-lost=list(map(int,input().split())) #도난
-reserve=list(map(int,input().split())) #여벌
-print(solution(n,lost,reserve))
+    #중복제거
+    s_lost=list(set(lost)-set(reserve)) 
+    s_reserve=list(set(reserve)-set(lost))
+    
+    for i in s_reserve:
+        #분실 학생 제거
+        if (i-1) in s_lost:
+            s_lost.remove(i-1) 
+        
+        #분실 학생 제거
+        elif (i+1) in s_lost:
+            s_lost.remove(i+1) 
+    #참여 학생        
+    answer=n-len(s_lost) 
+    return answer
