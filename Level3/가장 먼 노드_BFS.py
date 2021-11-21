eee7cb8bd714450f80d2408670e238f5607e034f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-21 11:33:56 +0900

    가장 먼 노드
    https://programmers.co.kr/learn/courses/30/lessons/49189

--- Level3/가장 먼 노드_BFS.py
+++ Level3/가장 먼 노드_BFS.py
@@ -2,18 +2,18 @@ from collections import deque
 
 #너비 우선 그래프 
 def BFS(graph, dist):
-    q=deque()
-    dist[1]=1
+    q = deque()
+    dist[1] = 1
     q.append(1)
 
     while q:
         cur = q.popleft()
 
         for i in graph[cur]:
+            #미방문이면
             if dist[i] == -1:
                 q.append(i)
                 dist[i] = dist[cur]+1
-
 
 def solution(n, edge):
     answer = 0
@@ -22,6 +22,7 @@ def solution(n, edge):
     #무향그래프
     graph = [[] for _ in range(len(edge)+1)]
 
+    #각 엣지를 연결
     for i in range(len(edge)):
         graph[edge[i][0]].append(edge[i][1])
         graph[edge[i][1]].append(edge[i][0])
