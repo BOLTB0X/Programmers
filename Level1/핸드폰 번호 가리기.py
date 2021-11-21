a750d89b9da7c9d35ef5dad56f8ea53889c4c929
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-21 15:39:54 +0900

    핸드폰 번호 가리기
    https://programmers.co.kr/learn/courses/30/lessons/12948?language=python3

--- Level1/핸드폰 번호 가리기.py
+++ Level1/핸드폰 번호 가리기.py
@@ -1,17 +1,15 @@
-#핸드폰 번호 가리기
-phone_number="01033334444"
 def solution(phone_number):
     answer=''
     cnt=0
-    for i in range(len(phone_number)-1,-1,-1): #거꾸로
-        if cnt<4: #4자리만 번호로 보여야하므로 cnt로 구분
+    
+    #거꾸로
+    for i in range(len(phone_number)-1,-1,-1):
+        #4자리만 번호로 보여야하므로 cnt로 구분 
+        if cnt<4: 
             answer+=phone_number[i]
             cnt+=1
         else:
             answer+='*'
-    return answer[::-1] #다시 거꾸로
-
-##다른 사람의 풀이
-def hide_numbers(s):
-    return "*"*(len(s)-4) + s[-4:]
-print(solution(phone_number))
+    
+    #다시 거꾸로
+    return answer[::-1] 
