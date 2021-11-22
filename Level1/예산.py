8aade9767dbf19b9ad249d922237c30c18c39414
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-22 15:33:40 +0900

    예산
    https://programmers.co.kr/learn/courses/30/lessons/12982?language=python3

--- Level1/예산.py
+++ Level1/예산.py
@@ -1,11 +1,16 @@
-#예산
 def solution(d, budget):
-    answer=0
-    d.sort() # 오름 차순으로 리스트 정렬
-    while budget < sum(d): # 리스트의 총합이 예산보다 크면 반복
-        d.pop() #제일 큰값을 빼라
-    return len(d)
+    answer = 0
+    
+    #오름차순 정렬
+    d.sort()
+    
+    #예산이 크다면
+    while budget < sum(d):
+        #가장 큰 값 제거
+        d.pop()
+    
+    #정답
+    answer = len(d)
+    
+    #반환
     return answer
-d=[1,3,2,5,4]
-budget=9
-print(solution(d,budget))
