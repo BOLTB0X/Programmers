aced8c09f2adcfb0889730fc67e54165be7e80fd
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-30 14:06:15 +0900

    크레인 인형뽑기 게임
    https://programmers.co.kr/learn/courses/30/lessons/64061?language=cpp

--- Level1/크레인 인형뽑기 게임.py
+++ Level1/크레인 인형뽑기 게임.py
@@ -1,14 +1,7 @@
-#크레인 인형뽑기 게임
-board=[[0,0,0,0,0],
-       [0,0,1,0,3],
-       [0,2,5,0,1],
-       [4,2,4,4,2],
-       [3,5,1,3,1]]
-moves=[1,5,3,5,1,2,1,4] #열 번호
-
 def solution(board, moves):
     answer = 0
     basket=[] #뽑은 인형을 담는 리스트
+       
     for move in moves: #하나씩 moves의 원소를
         for n in board: #행의 갯수만큼 반복
             if n[move-1] ==0: #비어있으면 다음 행으로
@@ -21,5 +14,5 @@ def solution(board, moves):
                     answer+=2
                 n[move-1]=0
                 break
+              
     return answer
-print(solution(board,moves))
