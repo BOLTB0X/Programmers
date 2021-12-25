3c06dc09a7331d6af247161cf377dce3df8df198
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-12-25 16:38:33 +0900

    구명보트
    https://programmers.co.kr/learn/courses/30/lessons/42885?language=cpp

--- Level2/구명보트.cpp
+++ Level2/구명보트.cpp
@@ -7,25 +7,24 @@ using namespace std;
 int solution(vector<int> people, int limit) {
     int answer = 0;
     
-    //오름차순 정렬
+    //그리디알고리즘을 위한 오름차순 정렬
     sort(people.begin(), people.end());
     
-    //이분탐색처럼 스타트와 끝을 정의
-    int start = 0;
-    int end = people.size() - 1;
-
-    //엇갈리기 전까지
-    while (start <= end) {
-        //둘다 데려가는 경우
-        if (people[start] + people[end] <= limit) {
-            start++;
-            end--;
-        }
+    //앞 뒤로 순차적으로 탐색
+    int front = 0, back = people.size() - 1;
+    while (front <= back) {
         //한놈만
-        else
-            end--;
+        if (people[front] + people[back] > limit) {
+            back--;
+        }
+        //둘다
+        else {
+            front++;
+            back--;
+        }
+        //카운트
         answer++;
     }
-
+    
     return answer;
 }
