15967311991163725e22c94c56822022462b8744
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-17 14:43:20 +0900

    크레인 인형뽑기 게임
    https://programmers.co.kr/learn/courses/30/lessons/64061

--- Level1/크레인 인형뽑기 게임.cpp
+++ Level1/크레인 인형뽑기 게임.cpp
@@ -6,30 +6,24 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
-    stack<int> basket;
+    stack<int> st;
 
-    //이동 명령 하나씩 확인
+    //명령어 확인
     for (int i = 0; i < moves.size(); ++i) {
-        //열 결정
-        int idx = moves[i] - 1;
-        //격자판 탐섹
-        for (auto& b : board) {
-            //인형 발견
-            if (b[idx] != 0) {
-                //득점이 가능한 경우
-                if (!basket.empty() && basket.top() == b[idx]) {
-                    basket.pop();
+        int cmd = moves[i];
+        
+        //열로 이동
+        for (int j = 0; j < board.size(); ++j) {
+            if (board[j][cmd] != 0) {
+                if (!st.empty() && st.top() == board[j][cmd])
                     answer += 2;
-                }
                 else
-                    basket.push(b[idx]);
-                //인형을 꺼냈으므로
-                b[idx] = 0;
+                    st.push(board[j][cmd]);
+                board[j][cmd] = 0;
                 break;
             }
         }
-       
     }
-    //반환
+
     return answer;
 }
