fc64e43f7739a1aaae1c9aa32fcb49858521fbb7
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-19 12:32:51 +0900

    타겟넘버
    https://programmers.co.kr/learn/courses/30/lessons/43165

--- Level2/타겟넘버.cpp
+++ Level2/타겟넘버.cpp
@@ -1,32 +1,27 @@
+#include <string>
 #include <vector>
 
 using namespace std;
 
-//DFS를 사용하기 때문에 전역 result
-int result=0;
+//전역변수로 카운트
+int cnt = 0;
 
-//깊이 우선 탐색을 이용한 백트래킹
-void DFS(vector<int> &numbers, int target, int tot, int depth) {
-    //numbers벡터까지 도달한다면 
-    if (depth == numbers.size()) {
-        //target으로 만들 수 있다면
+void DFS(vector<int>& numbers, int target, int level, int tot) {
+    if (level == numbers.size()) {
         if (tot == target)
-            result++;
+            cnt++;
         return;
     }
-    //각각 덧셈 뺄셈한 것들로 DFS호출
-    DFS(numbers,target,tot+numbers[depth],depth+1);
-    DFS(numbers,target,tot-numbers[depth],depth+1);
+    DFS(numbers, target, level+1, tot + numbers[level]);
+    DFS(numbers, target, level+1, tot - numbers[level]);
     return;
 }
 
 int solution(vector<int> numbers, int target) {
     int answer = 0;
     
-    //DFS로 탐색 시작
     DFS(numbers,target,0,0);
-    answer=result;
+    answer = cnt;
     
-    //정답 반환
     return answer;
 }
