bf1537ccc9c07e07f0ae35a52b2313777cce951d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-19 15:39:29 +0900

    체육복
    https://programmers.co.kr/learn/courses/30/lessons/42862

--- Level1/체육복.cpp
+++ Level1/체육복.cpp
@@ -5,40 +5,32 @@ using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
-    //체크용 벡터
-    vector<int> check(n, 1);
-
-    //잃어버린 학생들 감소
-    for (auto l : lost)
-        check[l - 1]--;
     
-    //여분이 있는 학생들 증가
-    for (auto r : reserve)
-        check[r - 1]++;
-
-    //체크 벡터 순회
-    for (int i = 0; i < check.size(); ++i) {
-        //만약 해당 학생이 체육복이 없다면
+    vector<int> check(n+1,1);
+    check[0] = 0;
+    
+    for (int i=0; i<lost.size(); ++i)
+        check[lost[i]]--;
+    
+    for (int i=0; i < reserve.size(); ++i)
+        check[reserve[i]]++;
+   
+    for (int i=1; i <= n; ++i) {
         if (check[i] == 0) {
-            //이전 사람 확인
-            if (i != 0 && check[i - 1] == 2) {
-                check[i - 1]--;
+            if (i != 1 && check[i-1] > 1) {
                 check[i]++;
-            }  
-
-            //다음 사람 확인
-            else if (i != check.size() - 1 && check[i + 1] == 2) {
-                check[i + 1]--;
+                check[i-1]--;
+            }
+            else if (i!=n && check[i+1] > 1) {
                 check[i]++;
+                check[i+1]--;
             }
         }
     }
-
-    //마지막 체육복이
-    for (auto c : check)
-        if (c != 0)
+    
+    for (int i=1;i<=n; ++i) {
+        if(check[i] >= 1)
             answer++;
-
-    //반환
+    }
     return answer;
 }
