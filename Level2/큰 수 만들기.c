399f32cc1ffd8d846251a6a9a6c2bdab75140314
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-24 14:38:10 +0900

    큰 수 만들기
    https://programmers.co.kr/learn/courses/30/lessons/42883

---
+++ Level2/큰 수 만들기.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(const char* number, int k) {
+    char* answer = (char*)malloc(sizeof(char) * strlen(number));
+    memset(answer, 0, sizeof(char) * strlen(number));
+
+    char max_num[1000001] = { 0, };
+    int num_idx = 0;
+
+    int size = strlen(number) - k;
+    int max_idx = 0;
+    int start = 0;
+
+    for (long long i = 0; i < size; ++i) {
+        max_idx = start;
+        char max_tmp = number[max_idx];
+
+        for (long long j = start + 1; j <= k + i; ++j) {
+            if (max_tmp < number[j]) {
+                max_tmp = number[j];
+                max_idx = j;
+            }
+        }
+
+        start = max_idx + 1;
+        max_num[num_idx++] = max_tmp;
+    }
+
+    strcpy(answer, max_num);
+    return answer;
+}
