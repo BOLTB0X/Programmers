49a94af7720cfcab2f3d0555cb5545c1ca38054d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-20 14:56:03 +0900

    가장 먼 노드_BFS 이용
    https://programmers.co.kr/learn/courses/30/lessons/49189

--- Level3/가장 먼 노드_BFS.cpp
+++ Level3/가장 먼 노드_BFS.cpp
@@ -6,67 +6,63 @@ using namespace std;
 int que[20001];
 int fr = 0, re = 0;
 
+//push
 void enqueue(int data) {
     que[re++] = data;
     return;
 }
 
+//pop
 int dequeue(void) {
     return que[fr++];
 }
 
 vector<int> adj[50001];
-vector<int> dist;
+vector<int> dist; // 거리 정보 저장용
 
-//깊이우선탐색
-int BFS(int n) {
-    int max_dist = -1;
-    enqueue(1);
-    dist[1] = 0;
+//너비우선탐색
+int BFS(int start) {
+    int max_dist = 1;
+    //1번으로 부터 멀리 떨어진 것이므로
+    dist[start] = 1;
+    enqueue(start);
     
-    //큐가 비어질때까지 
-    while(fr < re) {
+    //큐가 빌때까지
+    while (fr < re) {
         int cur = dequeue();
         
+        //인접 노드 순회
         for (int& next: adj[cur]) {
-            //재방문인 경우
-            if (dist[next] != -1)
+            if (dist[next] != 0)
                 continue;
-            //거리 정보
             dist[next] = dist[cur] + 1;
-            //최대인 경우 교체
-            if (max_dist < dist[next])
+            //최장 거리 교체
+            if (dist[next] > max_dist)
                 max_dist = dist[next];
             enqueue(next);
         }
     }
     
-    return max_dist;
-}
-
-//가장 먼 노드 거리를 가진 개수 반환
-int count_max_dist_Node(int max_value, int n) {
-    int cnt = 0;
-    for (int& d: dist) {
-        if (max_value == d)
-            cnt++;
-    }
-    
-    return cnt;
+    return max_dist; //반환
 }
 
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0, size = edge.size();
-    dist.resize(n + 1, -1);  
+    dist.resize(n + 1, 0);
     
-    //편의를 위한 그래프 생성
-    for (int i = 0; i< size; ++i) {
+    //인접리스트에 각 노드 정보를 확인
+    for (int i = 0; i < size; ++i) {
         adj[edge[i][0]].push_back(edge[i][1]);
-        adj[edge[i][1]].push_back(edge[i][0]);
+        adj[edge[i][1]].push_back(edge[i][0]); 
     }
     
-    int max_dist = BFS(n); //최대 거리 반환
-    answer = count_max_dist_Node(max_dist, n); //개수 반환
+    //최단경로로 이동했을 때 간선의 개수가 가장 많은 노드 -> BFS
+    int max_dist = BFS(1);
     
+    //1번을 제외한 최장 거리를 가진 노드 갯수 체크
+    for (int i = 2; i <= n; ++i) {
+        if (dist[i] == max_dist)
+            answer++;
+    }
     return answer;
 }
