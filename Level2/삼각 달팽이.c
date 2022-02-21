9a27735a9587508d164a79318e9cc5f29b0b726b
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-21 20:22:33 +0900

    삼각 달팽이
    https://programmers.co.kr/learn/courses/30/lessons/68645?language=c

--- Level2/삼각 달팽이.c
+++ Level2/삼각 달팽이.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#define ML 1001
 
-int board[1000][1000];
-//달팽이 방향
+int board[ML][ML];
 const int dy[3] = {1, 0, -1};
 const int dx[3] = {0, 1, -1};
 
 //초기화
 void init(int n) {
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) 
+        for (int j = 0; j < n; ++j)
             board[i][j] = 0;
     }
     return;
@@ -22,39 +22,40 @@ int in_Range(int y, int x, int n) {
 }
 
 int* solution(int n) {
-    //기본 세팅
-    int number = 1, y = 0, x = 0;
-    init(n);
+    int* answer = (int*)calloc(n * (n+1) / 2, sizeof(int));
+    int number = 1, idx = 0, cy = 0, cx = 0;
+    init(n); //초기화
     
-    //반복문으로 달팽이 방식으로 진행
-    while(in_Range(y, x, n) == 1 && board[y][x] == 0) {
+    //삼각달팽이 형태로 진행
+    while(in_Range(cy, cx, n) == 1 && board[cy][cx] == 0) {
         for (int dir = 0; dir < 3; ++dir) {
-            if (in_Range(y, x, n) == 0 || board[y][x] != 0)
+            //탈출조건
+            if (!in_Range(cy, cx, n) || board[cy][cx] != 0)
                 break;
             
+            //한 방향으로
             while(1) {
-                board[y][x] = number++;
-                int ny = y + dy[dir];
-                int nx = x + dx[dir];
+                board[cy][cx] = number++;
+                int ny = cy + dy[dir];
+                int nx = cx + dx[dir];
                 
+                //탈출 조건
                 if (in_Range(ny, nx, n) == 0 || board[ny][nx] != 0) {
-                    y += dy[(1 + dir) % 3];
-                    x += dx[(1 + dir) % 3];
+                    cy += dy[(1 + dir) % 3];
+                    cx += dx[(1 + dir) % 3];
                     break;
                 }
-                
-                y = ny, x = nx;
+        
+                cy = ny, cx = nx; //교체
             }
         }
     }
     
-    int* answer = (int*)calloc(n * (n+1) / 2, sizeof(int));
-    int answer_idx = 0;
-    //정답 
+    //정답 삽입
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (board[i][j] != 0)
-                answer[answer_idx++] = board[i][j];
+            if (board[i][j] != 0) //삼각달팽이 원소 삽입 
+                answer[idx++] = board[i][j];
         }
     }
     
