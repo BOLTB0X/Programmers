2fd776f6d8972ca4e7687edf28645941b67f43df
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-23 15:34:08 +0900

    n^2 배열 자르기
    https://programmers.co.kr/learn/courses/30/lessons/87390

---
+++ Level2/n^2 배열 자르기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int n, long long left, long long right) {
+    int* answer = (int*)malloc(sizeof(int) * (right - left + 1)); //길이
+    int idx = 0;
+
+    //규칙을 보면 
+    for (long long i = left; i <= right; ++i) {
+        int row = i / n;
+        int col = i % n;
+
+        //행과 열 값 중 더 큰 값 + 1로 해야함
+        int ele = row > col ? row + 1 : col + 1;
+        //정답 삽입
+        answer[idx++] = ele;
+    }
+    return answer;
+}
