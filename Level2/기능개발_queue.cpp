d1a58616a009a392d0efd862039a225f0b56b0a7
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-23 15:14:43 +0900

    기능개발
    https://programmers.co.kr/learn/courses/30/lessons/42586

--- Level2/기능개발_queue.cpp
+++ Level2/기능개발_queue.cpp
@@ -1,33 +1,43 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
+int que[100];
+int fr = 0, re = 0;
+
+void enqueue(int data) {
+    que[re++] = data;
+    return;
+}
+
+void dequeue(void) {
+    fr++;
+    return;
+}
+
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    queue<int> q;
     int size = progresses.size();
-
-    //일 번호 큐에 푸쉬
-    for (int i = 0; i < size; ++i)
-        q.push(i);
     
-    //연산 시작
-    while (!q.empty()) {
-        int cnt = 0;
+    //큐에 삽입
+    for (int i = 0; i < size; ++i)
+        enqueue(i);
+    
+    //큐가 비어질때까지
+    while (fr < re) {
+        int cnt = 0; //배포 갯수
         
-        //계속 진도율을 더해줌
-        for (int i = 0; i < size; ++i)
+        //진도율 업데이트
+        for (int i = 0; i < size; ++i) 
             progresses[i] += speeds[i];
-
-        //배포가능한지 체크
-        //최상단이 배포가능이 되어야함
-        while (!q.empty() && progresses[q.front()] >= 100) {
+        
+        //큐가 비어있지 않고 진도율이 100이 되었다면
+        while(fr < re && progresses[que[fr]] >= 100) {
             cnt++;
-            q.pop();
+            dequeue();
         }
-        if (cnt != 0)
+        if (cnt >= 1) //배포가 되었다면
             answer.push_back(cnt);
     }
     return answer;
