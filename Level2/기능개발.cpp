73056a64a116cafc0f7ba4818abbb09424e0deed
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-23 15:05:52 +0900

    기능개발
    https://programmers.co.kr/learn/courses/30/lessons/42586

--- Level2/기능개발.cpp
+++ Level2/기능개발.cpp
@@ -4,39 +4,32 @@
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
-    //작업 일수
-    vector<int> works;
-
-    //남은 진도율과 스피드와 나누어 떨어지는 지 비교
-    for (int i = 0;i < progresses.size(); ++i) {
-        if ((100 - progresses[i]) % speeds[i] == 0)
-            works.push_back((100 - progresses[i]) / speeds[i]);
+    int size = progresses.size();
+    vector<int> answer, works;
+    
+    //남은 작업량
+    for (int i = 0; i < size; ++i) {
+        int p = 100 - progresses[i];
+        if ((p % speeds[i]) == 0)
+            works.push_back(p / speeds[i]);
         else
-            works.push_back(((100 - progresses[i]) / speeds[i]) + 1);
+            works.push_back((p / speeds[i]) + 1);
     }
     
-    //비교용
-    int compare = works[0];
-    //배포 기능
-    int f=1;
+    //비교 및 카운트
+    int cur_work = works[0], cnt = 1;
     
-    for (int i=1; i<works.size(); ++i) {
-        //앞에 진도율이 끝나지 않았다면
-        //작업 한 후 배포이므로
-        if (compare >= works[i]) 
-            f++;
-        else {
-            //배포 후 비교랑 같이 배포되는 프로그램 갯수 초기화
-            answer.push_back(f);
-            f=1;
-            compare=works[i];
+    for (int i = 1; i <size; ++i) {
+        if (cur_work >= works[i])  //현재 작업량이 끝났다면
+            cnt++;
+        
+        else { //교체
+            answer.push_back(cnt);
+            cur_work = works[i];
+            cnt = 1;
         }
     }
+    answer.push_back(cnt);
     
-    //마지막 처리
-    answer.push_back(f);
-    
-    //반환
     return answer;
 }
