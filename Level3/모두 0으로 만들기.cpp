faac76e10add31b31894e9d7104743c62d10a85d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-24 16:01:03 +0900

    모두 0으로 만들기
    https://programmers.co.kr/learn/courses/30/lessons/76503

---
+++ Level3/모두 0으로 만들기.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long answer = 0;
+vector<long long> adj[300001]; //인접리스트로 변경
+
+long long Abs(long long a) {
+    return a < 0 ? -a : a;
+}
+
+void DFS(vector<long long>& tot, int cur, int par) {
+    for (long long& next : adj[cur]) {
+        if (next != par) //부모노드랑 다르다면
+            DFS(tot, next, cur);
+    }
+    // DFS 다 탐색 이후
+    tot[par] += tot[cur];
+    answer += Abs(tot[cur]);
+
+    return;
+}
+
+long long solution(vector<int> a, vector<vector<int>> edges) {
+    long long size = a.size();
+    vector<long long> tot(size, 0); //long long
+    for (long long i = 0; i < size; ++i)
+        tot[i] = a[i];
+
+    for (int i = 0; i < edges.size(); ++i) {
+        adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]);
+    }
+
+    DFS(tot, 0, 0); //깊이우선탐색
+
+    if (tot[0] == 0)
+        return answer;
+    return -1;
+}
