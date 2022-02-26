f0467ef2d168005d678a2ce981cf7d0549bd5cc0
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-26 10:36:00 +0900

    타겟 넘버
    https://programmers.co.kr/learn/courses/30/lessons/43165

--- Level2/타겟 넘버.cpp
+++ Level2/타겟 넘버.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 
-//전역변수로 카운트
 int cnt = 0;
 
 void DFS(vector<int>& numbers, int target, int level, int tot) {
+    //탈출 조건
     if (level == numbers.size()) {
-        if (tot == target)
+        if (tot == target) //조건 성립시
             cnt++;
         return;
     }
+    
     DFS(numbers, target, level+1, tot + numbers[level]);
     DFS(numbers, target, level+1, tot - numbers[level]);
     return;
@@ -20,7 +21,8 @@ void DFS(vector<int>& numbers, int target, int level, int tot) {
 int solution(vector<int> numbers, int target) {
     int answer = 0;
     
-    DFS(numbers,target,0,0);
+    //탐색 시작
+    DFS(numbers, target, 0,0);
     answer = cnt;
     
     return answer;
