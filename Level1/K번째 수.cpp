69753734f2777c2e47a30112d3fe9611e95b53d1
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-27 18:14:57 +0900

    K번째수
    https://programmers.co.kr/learn/courses/30/lessons/42748

--- Level1/K번째 수.cpp
+++ Level1/K번째 수.cpp
@@ -3,22 +3,15 @@
 
 using namespace std;
 
-//스와핑
-void swap(int *a, int *b) {
-    int *tmp=a;
-    a=b;
-    b=tmp;
-}
-
 //퀵정렬
-void quicksort(vector<int> &arr, int start, int end) {
+void quick_Sort(vector<int> &arr, int start, int end) {
     //원소가 한개인 경우
     if (start >= end)
         return;
     //피벗과 왼 오 셋팅
-    int pivot=start;
+    int pivot = start;
     int left = start+1;
-    int right =end;
+    int right = end;
     
     //엇갈릴때 까지 반복
     while (left <= right) {
@@ -34,31 +27,31 @@ void quicksort(vector<int> &arr, int start, int end) {
         else
             swap(arr[left],arr[right]);
     }
-    quicksort(arr,start,right-1);
-    quicksort(arr,right+1,end);
+    quick_Sort(arr,start,right-1);
+    quick_Sort(arr,right+1,end);
     return;
 }
 
-//배열 슬라이딩
-int sliding(vector<int> &array, int start, int end, int target) {
-    vector<int> v;
-    //벡터에 슬라이딩 된 원소들 삽입
-    for (int i=start-1; i<end;i++) 
-        v.push_back(array[i]);
-    //정렬
-    quicksort(v,0,v.size()-1);
-    return v[target-1];
+//요구되로 원소 구하기
+int vector_sliding(vector<int> &array, int start, int end, int target) {
+    vector<int> tmp;
+    for (int i = start - 1; i < end; ++i) {
+        tmp.push_back(array[i]);
+    }
+    
+    quick_Sort(tmp, 0, tmp.size() - 1); //정렬
+    return tmp[target-1];
 }
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    //먼저 커맨드의 요청에 따라 배열을 잘라야함
-    //임시벡터 생성 후 임시벡터에 삽입
-    for (int i=0;i<commands.size();i++) {
-        //열은 3으로 고정
-        //배열 슬라이딩 호출
-        int ret = sliding(array,commands[i][0],commands[i][1],commands[i][2]);
+    
+    //명령대로 배열 원소 구함
+    for (auto& cmd : commands) {
+        int ret = vector_sliding(array, cmd[0],cmd[1], cmd[2]);
         answer.push_back(ret);
     }
+    
+    //반환
     return answer;
 }
