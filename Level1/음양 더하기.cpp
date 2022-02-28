7564d0e16494d7b56abae77f0bc72c8a37ec2ee1
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-28 15:12:43 +0900

    음양 더하기
    https://programmers.co.kr/learn/courses/30/lessons/76501?language=cpp

--- Level1/음양 더하기.cpp
+++ Level1/음양 더하기.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int solution(vector<int> absolutes, vector<bool> signs) {
     int answer = 0;
-    for (int i = 0; i < absolutes.size(); i++) {
-        if (signs[i])
+    
+    //순회
+    for (int i=0;i<absolutes.size();i++) {
+        if (signs[i]) //맞다면
             answer += absolutes[i];
-        else
+        else //틀리다면
             answer -= absolutes[i];
     }
     return answer;
