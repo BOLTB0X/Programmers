8422e76ba4f9a5a61d6e95e74855623dfeb814fa
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-01 16:13:24 +0900

    2개 이하로 다른 비트
    https://programmers.co.kr/learn/courses/30/lessons/77885

--- Level2/2개 이하로 다른 비트.cpp
+++ Level2/2개 이하로 다른 비트.cpp
@@ -6,14 +6,21 @@ using namespace std;
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
     long long size = numbers.size();
-
-    for (long long& number : numbers) {
+    
+    for (long long& number: numbers) {
         if (number % 2 == 0)
             answer.push_back(number + 1);
         else {
-            long long last_Zero = (number + 1) & (-number);
-            long long result = (number | last_Zero) & (~(last_Zero >> 1));
-            answer.push_back(result);
+            long long bit = 1;
+            // 가장 오른쪽부터 차례로 n 개의 연속된 1 로 이루어진 비트구하기
+            while (1) {
+                if ((numbers[i] & bit) == 0) break;
+                bit *= 2; // 곱하기 2 를 하면 다음 비트로
+            }
+            bit /= 2;
+
+            // 구한 비트 더해주기
+            answer.push_back(numbers[i] + bit);
         }
     }
     return answer;
