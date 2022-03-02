f1d5e402648d2db718dc4c51301ffaa3dab47404
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-02 17:21:08 +0900

    쿼드압축 후 개수 세기
    https://programmers.co.kr/learn/courses/30/lessons/68936

--- Level2/쿼드압축 후 개수 세기.c
+++ Level2/쿼드압축 후 개수 세기.c
@@ -4,10 +4,12 @@
 
 int z_cnt = 0, o_cnt = 0;
 
-void compassion(int** arr, int y, int x, int size, int cmp) {
-    int half = size / 2, flag = 1;
+//재귀를 통한 분할 정복으로 압축
+void compasstion(int** arr, int y, int x, int size, int cmp) {
+    int half = size / 2;
+    int flag = 1; //체크용
     
-    //재귀를 활용한 분할탐색의 탈출 조건
+    //탈출 조건
     if (size < 2) {
         if (cmp == 1)
             o_cnt++;
@@ -16,9 +18,10 @@ void compassion(int** arr, int y, int x, int size, int cmp) {
         return;
     }
     
-    for (int i = y; i <  y + size; ++i) {
+    //분할 가능한지 탐색
+    for (int i = y; i < y + size; ++i) {
         for (int j = x; j < x + size; ++j) {
-            if (cmp != arr[i][j]) {
+            if (arr[i][j] != cmp) {
                 flag = 0;
                 break;
             }
@@ -27,7 +30,7 @@ void compassion(int** arr, int y, int x, int size, int cmp) {
             break;
     }
     
-    //분할이 정상적으로 진행된 것이므로 
+    //분할 가능하다면
     if (flag == 1) {
         if (cmp == 1)
             o_cnt++;
@@ -35,11 +38,17 @@ void compassion(int** arr, int y, int x, int size, int cmp) {
             z_cnt++;
         return;
     }
-    //분할탐색 시작
-    compassion(arr, y, x, half, arr[y][x]); //범위 
-    compassion(arr, y + half, x, half, arr[y+half][x]); //한칸
-    compassion(arr, y, x + half, half, arr[y][x + half]); //한칸
-    compassion(arr, y + half, x + half, half, arr[y + half][x + half]); //대각선
+    
+    //분할 시작
+    //1 범위를 반으로
+    compasstion(arr, y, x, half, arr[y][x]);
+    //2 x축만
+    compasstion(arr, y, x + half, half, arr[y][x + half]);
+    //3 y축만
+    compasstion(arr, y + half, x, half, arr[y + half][x]);
+    //4 다
+    compasstion(arr, y + half, x + half, half, arr[y + half][x + half]);
+    
     return;
 }
 
@@ -47,7 +56,9 @@ int* solution(int** arr, size_t arr_rows, size_t arr_cols) {
     int* answer = (int*)malloc(sizeof(int) * 2);
     answer[0] = 0, answer[1] = 0;
     
-    compassion(arr, 0, 0, arr_rows, arr[0][0]);
-    answer[0] = z_cnt, answer[1] = o_cnt;
+    //분할정복으로 압축 시작
+    compasstion(arr, 0, 0, arr_rows, arr[0][0]);
+    answer[0] = z_cnt;
+    answer[1] = o_cnt;
     return answer;
 }
