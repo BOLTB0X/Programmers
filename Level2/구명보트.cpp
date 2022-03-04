0c5566c3ab5687d0bbe41ae767628f7633d39e7e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-04 16:07:00 +0900

    구명 보트
    https://programmers.co.kr/learn/courses/30/lessons/42885

--- Level2/구명보트.cpp
+++ Level2/구명보트.cpp
@@ -6,25 +6,21 @@ using namespace std;
 
 int solution(vector<int> people, int limit) {
     int answer = 0;
+    int size = people.size();
+
+    int left = 0, right = size - 1;
+    sort(people.begin(), people.end()); //오름 차순
     
-    //그리디알고리즘을 위한 오름차순 정렬
-    sort(people.begin(), people.end());
-    
-    //앞 뒤로 순차적으로 탐색
-    int front = 0, back = people.size() - 1;
-    while (front <= back) {
-        //한놈만
-        if (people[front] + people[back] > limit) {
-            back--;
-        }
+    while(left <= right) {
+        //한명만 데리고 가는 경우
+        if (people[left] + people[right] > limit) 
+            right--;
         
-        //둘다
+        //둘 다
         else {
-            front++;
-            back--;
+            left++;
+            right--;
         }
-        
-        //카운트
         answer++;
     }
     
