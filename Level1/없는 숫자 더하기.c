da715551e8a50a56dc8925b83f2e15c4d3fb5119
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-07 16:22:29 +0900

    없는 숫자 더하기
    https://programmers.co.kr/learn/courses/30/lessons/86051

--- Level1/없는 숫자 더하기.c
+++ Level1/없는 숫자 더하기.c
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-//배열안에 있는 지 체크
-int is_arr(int numbers[], size_t numbers_len, int n) {
+//배열 안에 원소가 있는지
+int is_ele_in_arr(int *numbers, size_t numbers_len, int n) {
     for (int i = 0; i < numbers_len; ++i) {
         if (numbers[i] == n)
             return 1;
@@ -16,7 +16,7 @@ int solution(int numbers[], size_t numbers_len) {
     
     for (int i = 0; i <= 9; ++i) {
         //배열안에 원소가 있는 가?
-        if (is_arr(numbers, numbers_len, i) == 0)
+        if (is_ele_in_arr(numbers, numbers_len, i) == 0)
             answer += i;
     }
     return answer;
