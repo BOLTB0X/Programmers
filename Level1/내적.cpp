6c061bd7196b5d5873d93b1b537797165f932f8b
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-08 15:46:54 +0900

    내적
    https://programmers.co.kr/learn/courses/30/lessons/70128?language=cpp

--- Level1/내적.cpp
+++ Level1/내적.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 int solution(vector<int> a, vector<int> b) {
     int answer = 0;
-    for (int i = 0; i < a.size(); i++) {
+    int size = a.size(); //내적 조건상 b랑 같음
+    
+    //내적 연산
+    for (int i = 0; i < size ;i++) 
         answer += (a[i] * b[i]);
-    }
+    
     return answer;
 }
