2b0fc67f10ea242706d19e1491ca784f62fbf77c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-11 01:53:05 +0900

    체육복
    https://programmers.co.kr/learn/courses/30/lessons/42862

--- Level1/체육복.cpp
+++ Level1/체육복.cpp
@@ -5,32 +5,38 @@ using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
+    vector<int> cnt(n + 1, 1); //카운트 벡터 생성 및 초기화
     
-    vector<int> check(n+1,1);
-    check[0] = 0;
+    //잃어버린 학생들 감소
+    for (int& l: lost) 
+        cnt[l]--;
     
-    for (int i=0; i<lost.size(); ++i)
-        check[lost[i]]--;
+    //여분이 있는 학생들 증가
+    for (int& r: reserve)
+        cnt[r]++;
     
-    for (int i=0; i < reserve.size(); ++i)
-        check[reserve[i]]++;
-   
-    for (int i=1; i <= n; ++i) {
-        if (check[i] == 0) {
-            if (i != 1 && check[i-1] > 1) {
-                check[i]++;
-                check[i-1]--;
+    //해당 학생을 카운트
+    for (int i = 1; i <= n; ++i) {
+        //체육복이 없는 학생 발견
+        if (cnt[i] == 0) {
+            //첫 번째 학생이 아니고 <- 쪽에 여분이 있다면
+            if (i != 1 && cnt[i - 1] >= 2) {
+                cnt[i]++;
+                cnt[i - 1]--;
             }
-            else if (i!=n && check[i+1] > 1) {
-                check[i]++;
-                check[i+1]--;
+            //마지막 학생이 아니고 -> 쪽에 여분이 있다면
+            else if (i != n && cnt[i + 1] >= 2) {
+                cnt[i]++;
+                cnt[i + 1]--;
             }
         }
     }
     
-    for (int i=1;i<=n; ++i) {
-        if(check[i] >= 1)
+    //총 학생들 카운트
+    for (int i = 1; i <= n; ++i) {
+        if (cnt[i] >= 1)
             answer++;
     }
+    
     return answer;
 }
