d8667acadb347b28b0f4337b587db53fb5d512d6
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-10 14:33:39 +0900

    괄호 회전하기
    https://programmers.co.kr/learn/courses/30/lessons/76502?language=cpp

--- Level2/괄호 회전하기.cpp
+++ Level2/괄호 회전하기.cpp
@@ -16,52 +16,61 @@ void pop(void) {
     return;
 }
 
-//왼쪽 방향 회전
-string rotate_str(string s) {
-    int size = s.length();
-    char tmp = s[0];
+//괄호가 올바른지 체크
+int is_Correct(string str) {
+    int size = str.length(); //길이
     
-    for (int i = 1;  i < size; ++i) 
-        s[i - 1] = s[i];
-    s[size - 1] = tmp;
-    
-    return s;
-}
-
-//올바른 지 체크
-int is_correct(string s) {
-    int size = s.length();
-    
-    for (int i = 0; i < size; ++i) {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
-            push(s[i]);
+    //스택을 이용하며 탐색 시작
+    for (int i =0; i < size; ++i) {
+        //열림이면
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[')
+            push(str[i]);
         else {
-            if (s[i] == ')' && stack[top] == '(')
+            //pop을 할 수 있는 경우들
+            if (str[i] == ')' && stack[top] == '(')
                 pop();
-            else if (s[i] == '}' && stack[top] == '{')
+            else if (str[i] == '}' && stack[top] == '{')
                 pop();
-            else if (s[i] == ']' && stack[top] == '[')
+            else if (str[i] == ']' && stack[top] == '[')
                 pop();
+            //맞지 않다면 -> 올바른 X
             else
                 return 0;
         }
     }
-    //연산이 끝났는데도 스택이 비어지지 않았다면
+    //스택이 비어있지 않다면 올바르지 X
     if (top != -1)
         return 0;
     return 1;
+}
+
+//문자열 회전
+string rotate_str(string str) {
+    int size = str.length();
+    char tmp = str[0];
+    
+    //왼쪽으로 회전
+    for (int i = 1; i < size; ++i) 
+        str[i - 1] = str[i];
+    
+    str[size - 1] = tmp;
+    
+    return str;
 }
 
 int solution(string s) {
     int answer = 0;
     int size = s.length();
     
-    if (is_correct(s) == 1) 
+    if (is_Correct(s) == 1) 
         answer++;
     
-    for (int i = 1; i < size; ++i) {
-        s = rotate_str(s); //괄호 회전
-        if (is_correct(s) == 1) //올바른가?
+    for (int i = 1; i <size; ++i) {
+        s = rotate_str(s); //왼쪽으로 회전
+        top = -1; //스택 초기화
+        
+        //올바른 가?
+        if (is_Correct(s) == 1)
             answer++;
     }
     
