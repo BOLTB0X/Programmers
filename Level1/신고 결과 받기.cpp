3860421d1c24270c8c7233db956f61d6a4f4adf0
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-16 00:54:29 +0900

    신고 결과 받기
    https://programmers.co.kr/learn/courses/30/lessons/92334

--- Level1/신고 결과 받기.cpp
+++ Level1/신고 결과 받기.cpp
@@ -1,83 +1,72 @@
-#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int score[1000][1000];
-int sum[1000];
-
-void init(int len) {
-    for (int i = 0; i < len; ++i) {
-        for (int j = 0; j < len; ++j)
-            score[i][j] = 0;
+int count_out(vector<vector<int>> &table, int size, int a, int k) {
+    int ret = 0;
+    for (int i = 0; i < size; ++i) {
+        int cnt = 0;
+        if (table[a][i] == 1) {
+            for (int j = 0; j < size; ++j) {
+                if (table[j][i] == 1)
+                    cnt++;
+            }
+            
+            if (cnt >= k)
+                ret++;
+        }
     }
-    for (int i = 0; i < len; ++i)
-        sum[i] = 0;
-    return;
-}
-
-vector<string> get_split(string report) {
-    vector<string> v;
-    int pos = report.rfind(' ');
-
-    v.push_back(report.substr(0, pos));
-    v.push_back(report.substr(pos + 1));
-
-    return v;
-}
-
-void count_score(vector<string>& id_list, vector<string>& st) {
-    int row = 0, col = 0;
-    for (int i = 0; i < id_list.size(); ++i) {
-        if (id_list[i] == st[0])
-            row = i;
-        else if (id_list[i] == st[1])
-            col = i;
-    }
-
-    //중복신고 방지
-    if (score[row][col] == 0) {
-        score[row][col]++;
-        sum[col]++;
-    }
-
-    return;
+    
+    //신고 한넘이 정지시킨 넘 갯수
+    return ret;
 }
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer;
-    //편의용
-    int len = id_list.size();
-    //초기화
-    init(len);
-
-    for (int i = 0; i < report.size(); ++i) {
-        vector<string> str = get_split(report[i]);
-        count_score(id_list, str);
-    }
-
-    for (int i = 0; i < len; ++i) {
-        int cnt = 0;
-        for (int j = 0; j < len; ++j) {
-            //신고 1번하고 누적횟수 조건 확인
-            if (score[i][j] > 0 && sum[j] >= k) 
-                cnt++;
+    int size = id_list.size();
+    
+    // size X size 표, 득점 생성
+    vector<vector<int>> table(size, vector<int> (size, 0));
+    vector<int> score(size, 0);
+    
+    //신고자와 받은자 찾기
+    for (string& s: report) {
+        int idx = s.find(' ');
+        string atteck = s.substr(0,idx);
+        string defence = s.substr(idx + 1);
+        int row, col;
+        
+        //아이디 찾기
+        for (int i = 0; i< size; ++i) {
+            if (atteck == id_list[i]) 
+                row = i;
+            else if (defence == id_list[i])
+                col = i;
         }
+        
+        //중복 허용 X
+        if (table[row][col] == 0) {
+            table[row][col] = 1;
+            score[col]++;
+        }
+    }
+    /*
+    for (int i = 0; i < size; ++i) {
+        int cnt = count_out(table, size, i , k);
         answer.push_back(cnt);
     }
-
-    return answer;
-}
-
-
-int main(void) {
-    vector<string> id_list = { "muzi", "frodo", "apeach", "neo" };
-    vector<string> report = { "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" };
-
-    vector<int> ret = solution(id_list, report, 2);
-
-    for (int i = 0; i < ret.size(); ++i) {
-        cout << ret[i] << ' ';
+    */
+    
+    //신고자 행, 받은자 열
+    for (int i = 0; i < size; ++i) {
+        int cnt = 0;
+        for (int j = 0; j < size; ++j) {
+            if (table[i][j] > 0 && score[j] >= k)
+                   cnt++;
+        }
+        answer.push_back(cnt); 
     }
+    
+    return answer;
 }
