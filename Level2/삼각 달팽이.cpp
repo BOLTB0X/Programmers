464d48ddd166008cdff10032349b67095144f520
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-16 17:09:15 +0900

    삼각 달팽이
    https://programmers.co.kr/learn/courses/30/lessons/68645

--- Level2/삼각 달팽이.cpp
+++ Level2/삼각 달팽이.cpp
@@ -3,46 +3,54 @@
 
 using namespace std;
 
+//게시판
 vector<vector<int>> board;
+//달팽이식 이동방향
+const int dy[3] = {1, 0, -1};
+const int dx[3] = {0, 1, -1};
 
-const int dy[3] = { 1,0,-1 };
-const int dx[3] = { 0,1,-1 };
-
-int in_Range(int y, int x, int n) {
-    return 0 <= y && y < n && 0 <= x && x < n;
+//범위
+int in_Range(int n, int y, int x) {
+    return 1<= y && y <= n && 1<= x && x <= n;
 }
 
 vector<int> solution(int n) {
     vector<int> answer;
-    board.resize(n, vector<int>(n, 0));
-    int number = 1, cy = 0, cx = 0;
-
-    while (in_Range(cy, cx, n) == 1 && board[cy][cx] == 0) {
-        for (int dir = 0; dir < 3; ++dir) {
-            if (in_Range(cy, cx, n) == 0 || board[cy][cx] != 0)
+    board.resize(n + 1, vector<int>(n + 1, 0));
+    int number = 1;
+    int y = 1, x = 1;
+    
+    //시뮬
+    while(in_Range(n, y, x) == 1 && board[y][x] == 0) {
+        for (int dir =0; dir < 3; ++dir) {
+            //탈출 조건
+            if (in_Range(n, y, x) == 0 || board[y][x] != 0)
                 break;
-
-            while (1) {
-                board[cy][cx] = number++;
-                int ny = cy + dy[dir];
-                int nx = cx + dx[dir];
-
-                if (in_Range(ny, nx, n) == 0 || board[ny][nx] != 0) {
-                    cy += dy[(1 + dir) % 3];
-                    cx += dx[(1 + dir) % 3];
+            
+            while(1) {
+                board[y][x] = number++;
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+                
+                //탈출 조건
+                if (in_Range(n, ny, nx) == 0 || board[ny][nx] != 0) {
+                    y += dy[(1 + dir) % 3];
+                    x += dx[(1 + dir) % 3];
                     break;
                 }
-
-                cy = ny, cx = nx;
+                
+                y = ny, x = nx;
             }
         }
     }
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    
+    //정답 삽입
+    for (int i=1; i<=n;++i) {
+        for (int j=1; j <=n; ++j) {
             if (board[i][j] != 0)
                 answer.push_back(board[i][j]);
         }
     }
+    
     return answer;
 }
