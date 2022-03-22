8cff8ce3cc6b6b7c2004c8fcaae5d2484f400c23
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-22 17:42:07 +0900

    큰 수 만들기
    https://programmers.co.kr/learn/courses/30/lessons/42883

--- Level2/큰 수 만들기.cpp
+++ Level2/큰 수 만들기.cpp
@@ -5,24 +5,22 @@ using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    int size = number.length();
-    int start_idx = 0;
+    int size = number.length(); // 길이
     
-    //그리드 알고리즘
-    //범위 셋팅 조심
+    // 탐색 시작
+    int start_idx= 0; // 시작 인덱스 설정
+    // 총 2개를 지우고 만들 수 있는 수 
     for (int i = 0; i < size - k; ++i) {
-        int max_idx = start_idx;
-        
-        //현재 구간에서 최댓값 탐색
-        for (int j = start_idx; j <= k + i; ++j) {
-            if (number[max_idx] < number[j]) 
+        int max_idx = start_idx; // 최댓값을 알기위해 시작점으로
+        // 그리디로 큰 값 체크
+        for (int j = start_idx; j <= k + i; ++j) {            
+            if (number[max_idx] < number[j])
                 max_idx = j;
         }
         
-        //최댓값 셋팅 됐으므로
-        start_idx = max_idx + 1; //찾은 최댓값 다음 부터 다시 시작
+        // 시작 위치를 현재 최댓값 뒤로 셋팅
+        start_idx = max_idx + 1;
         answer += number[max_idx];
     }
-    
     return answer;
 }
