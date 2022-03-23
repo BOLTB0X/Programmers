e97e9597c8ee72d22eae4aa46755114944d9f45b
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-23 20:31:41 +0900

    프린터
    https://programmers.co.kr/learn/courses/30/lessons/42587

--- Level2/프린터_Priority_Queue.cpp
+++ Level2/프린터_Priority_Queue.cpp
@@ -1,51 +1,38 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <queue> // queue, priority_queue
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    //우선순위 큐 선언
-    priority_queue<int> pq;
+    int size = priorities.size(); // 길이
+    queue<int> que;
+    priority_queue<int> pq; 
     
-    //큐 선언, 우선순위와 기존 위치=idx까지
-    queue<pair<int,int>> q;
-    
-    //우선순위를 pq,q에 옮겨줌
-    for (int i=0;i<priorities.size();++i) {
+    for (int i = 0; i<size; ++i) {
         pq.push(priorities[i]);
-        q.push({priorities[i],i});
+        que.push(i);
     }
     
-    //우선순위 큐가 내림차순으로 정렬됨
-    //원하는 위치가 몇번째로 뽑히는 지가 궁금하므로
-    //일반 큐의 i와 원하는 위치를 비교
-    int order = 1;
-    while (!q.empty()) {
-        //같이 크거나 같다면 
-        if (pq.top() <= q.front().first) {
-            // 원하는 위치까지 같다면
-            if(location == q.front().second)
-                //반복문 중단
+    while(!que.empty()) {
+        int idx = que.front();
+        que.pop();
+        
+        // 우선순위가 가장 높다면
+        if(priorities[idx] == pq.top()) {
+            answer++; // 배포
+            
+            if (idx == location) // 원하는 것이면
                 break;
-            //위치가 다르다면
-            order++;
-            pq.pop();
-            q.pop();
+            
+            else // 아니면
+                pq.pop();
         }
         
-        //작다면
-        else {
-            //큐를 회전시킴
-            q.push(q.front());
-            q.pop();
-        }
+        else // 다시 넣어둠
+            que.push(idx);
     }
     
-    //정답 교체
-    answer = order;
-    
-    //반환
     return answer;
 }
