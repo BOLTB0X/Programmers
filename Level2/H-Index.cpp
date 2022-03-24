2e556d75b5cc26e070a334eb589fbcd447f4c09e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-24 16:10:15 +0900

    H_Index
    https://programmers.co.kr/learn/courses/30/lessons/42747

--- Level2/H-Index.cpp
+++ Level2/H-Index.cpp
@@ -1,52 +1,26 @@
 #include <string>
 #include <vector>
+#include <algorithm> // sort
 
 using namespace std;
 
-//퀵정렬
-void quick_Sort(vector<int> &arr, int start, int end) {
-    //만약 원소가 하나라면
-    if (start >= end)
-        return;
-    int pivot = start;
-    int left = pivot+1;
-    int right = end;
-    
-    //엇갈리기 전까지
-    while (left <= right) {
-        //피벗보다 큰 값을 만날때 까지
-        while (left <= end && arr[left] <= arr[pivot]) 
-            left++;
-        //피벗보다 작은 값을 만날 때 까지
-        while (right > start && arr[right] >= arr[pivot])
-            right--;
-        //만약 엇갈렸다면
-        if (left > right) 
-            swap(arr[pivot],arr[right]);
-        else
-            swap(arr[left],arr[right]);
-    }
-    quick_Sort(arr, start, right-1);
-    quick_Sort(arr, right+1, end);
-    return;
-}
-
 int solution(vector<int> citations) {
     int answer = 0;
-    int size = citations.size(), h_idx = 0;
+    int h_idx = citations.size();
+    // h번 이상 나머지 논문은 h번이하 인용
+    // 즉 인덱스를 길이로 할당하고 
+    // 순차적으로 줄여가며 작거나 같아지는 지점이 정답
     
-    quick_Sort(citations, 0, size - 1); //오름차순
+    // 오름차순 정렬
+    sort(citations.begin(), citations.end());
     
-    //정렬된 벡터에서 길이와 그 인텍스를 뺀 값이 원소보다 크면 그 값이
-    //h_index
-    for (int i = 0; i < size; ++i) {
-        //즉 깊이 들어갈 수록 h_idx값은 줄어든다.
-        h_idx = size - i;
-        //H-idx를 알았으니
-        if (citations[i] >= h_idx) {
-            answer = h_idx;
-            break; //안 멈추면 계속 작아짐
-        }
+    // 탐색
+    for (int i = 0; i < citations.size(); ++i) {
+        if (h_idx <= citations[i]) 
+            break;
+        h_idx--;
     }
+    
+    answer = h_idx;
     return answer;
 }
