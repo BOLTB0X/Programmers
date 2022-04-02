5fbf6981ba33e9858ee3bce3e7905fa7fe29e4bc
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-02 11:14:28 +0900

    단어 변환
    https://programmers.co.kr/learn/courses/30/lessons/43163

--- Level3/단어 변환.cpp
+++ Level3/단어 변환.cpp
@@ -3,39 +3,41 @@
 
 using namespace std;
 
-int result = 51;
+int result = 51; // 최소를 위한
+vector<int> visited; // 방문리스트
 
-// 최솟값반환
+// 최솟값 반환
 int Min(int a, int b) {
     return a < b ? a : b;
 }
 
-// 단어 차이 확인
-int is_trans(string a, string b) {
+// 1개만 차이가 나는지
+bool is_trans_condi(string a, string b) {
     int cnt = 0;
-    for (int i=0;i<a.length();++i) {
+    for (int i = 0; i < a.length(); ++i) {
         if (a[i] != b[i])
             cnt++;
-        if (cnt == 2)
-            return 0;
-    }
-    return 1;
-}
-
-void DFS(vector<string> &words, vector<int> &visited, string cur, string target, int level) {
-    // 목표에 도달 했다면
-    if (cur == target) {
-        result = Min(result,level);
-        return;
     }
     
-    for (int i = 0; i < words.size(); ++i) {
-        // 반환이 가능하다면
-        if (is_trans(cur,words[i]) == 1) {
-            if (visited[i] == 1)
+    return cnt == 1 ? 1 : 0;
+}
+
+// 깊이우선탐색
+void DFS(vector<string>& words, int size, string cur, string target, int level) {
+    // 탈출 조건
+    if (cur == target) {
+        result = Min(result, level);
+        return;
+    }
+        
+    for (int i = 0; i < size;++i) {
+        // 조건
+        if (is_trans_condi(cur, words[i]) == 1) {                  
+            if (visited[i] == 1) // 재방문 방지
                 continue;
+        
             visited[i] = 1;
-            DFS(words,visited,words[i],target,level+1);
+            DFS(words, size, words[i], target, level + 1);
             visited[i] = 0;
         }
     }
@@ -44,11 +46,12 @@ void DFS(vector<string> &words, vector<int> &visited, string cur, string target,
 
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
-    vector<int> visited(words.size(),0); // 방문리스트 초기화
+    int size = words.size(); // 길이
+    visited.resize(size, 0); // 초기화
     
-    DFS(words, visited, begin, target, 0);
-    
-    if (result != 51) 
+    DFS(words, size, begin, target, 0);
+    // 도달 가능하다면
+    if (result != 51)
         answer = result;
     return answer;
 }
