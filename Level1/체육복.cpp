e87ca0f4611a8ce0e309a4e11bb7b343f759ee13
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-11 15:10:38 +0900

    체육복
    https://programmers.co.kr/learn/courses/30/lessons/42862

--- Level1/체육복.cpp
+++ Level1/체육복.cpp
@@ -5,38 +5,53 @@ using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
-    vector<int> cnt(n + 1, 1); //카운트 벡터 생성 및 초기화
+    vector<int> cnt(n+1, 1); // 여분
     
-    //잃어버린 학생들 감소
-    for (int& l: lost) 
+    // 분실
+    for (int& l : lost) 
         cnt[l]--;
     
-    //여분이 있는 학생들 증가
+    // 여분
     for (int& r: reserve)
         cnt[r]++;
     
-    //해당 학생을 카운트
-    for (int i = 1; i <= n; ++i) {
-        //체육복이 없는 학생 발견
-        if (cnt[i] == 0) {
-            //첫 번째 학생이 아니고 <- 쪽에 여분이 있다면
-            if (i != 1 && cnt[i - 1] >= 2) {
-                cnt[i]++;
-                cnt[i - 1]--;
+    // 그리디
+    for (int i = 1; i <=n; ++i) {
+        // 해당 학생은 참가 가능
+        if (cnt[i] != 0)
+            answer++;
+        
+        // 빌려야하는 학생이면
+        else if (cnt[i] == 0){
+            // 그 학생이 1번이면
+            if (i == 1) {
+              if (cnt[i + 1] > 1) {
+                  answer++;
+                  cnt[i + 1]--;
+              }
             }
-            //마지막 학생이 아니고 -> 쪽에 여분이 있다면
-            else if (i != n && cnt[i + 1] >= 2) {
-                cnt[i]++;
-                cnt[i + 1]--;
+            // 그 학생이 맨 뒤이면
+            else if (i == n) {
+                if (cnt[i - 1] > 1) {
+                  answer++;
+                  cnt[i - 1]--;
+              }
+            }
+            
+            // 나머지
+            else {
+                // 왼쪽 학생 체크
+                if (cnt[i - 1] > 1) {
+                  answer++;
+                  cnt[i - 1]--;
+              }
+                // 오른쪽 학생 체크
+                else if (cnt[i+1] > 1){
+                    answer++;
+                    cnt[i + 1]--;
+                }
             }
         }
     }
-    
-    //총 학생들 카운트
-    for (int i = 1; i <= n; ++i) {
-        if (cnt[i] >= 1)
-            answer++;
-    }
-    
     return answer;
 }
